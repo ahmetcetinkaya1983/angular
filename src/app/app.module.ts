b57ded3b9b24aebfca8b563ce3f71a87f7e141a4
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-02-10 12:41:25 +0300

    angular flex, hammerjs, material configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
